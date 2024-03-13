3e83c63f703864e55df4e0d0e367c48eacb2003d
KerimYitmez <161705119+KerimYitmez@users.noreply.github.com>
2024-03-13 14:03:43 +0100

    rechter widget eingebaut

--- lib/startseite/startseite.dart
+++ lib/startseite/startseite.dart
@@ -6,219 +6,96 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPageState extends State<StartPage> {
+  double _opacity =
+      0.5; // Transparenz des Widgets, 0.0 ist völlig transparent, 1.0 ist völlig sichtbar
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'eGadget',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('eGadget Home Page'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.account_circle),
-              onPressed: () {
-                // Profil-Icon Aktion
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.access_time),
-              onPressed: () {
-                // Uhr-Icon Aktion
-              },
-            ),
-          ],
+        title: 'eGadget',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-        drawer: Drawer(
-          // Menü auf der linken Seite
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Text('Menü'),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
+        home: Stack(children: <Widget>[
+          Scaffold(
+            appBar: AppBar(
+              title: Text('eGadget Startseite'),
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.account_circle),
+                  onPressed: () {
+                    // Profil-Icon Aktion
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.access_time),
+                  onPressed: () {
+                    // Uhr-Icon Aktion
+                  },
+                ),
+              ],
+            ),
+            drawer: Drawer(
+              // Menü auf der linken Seite
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  DrawerHeader(
+                    child: Text('Menü'),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 13, 106, 181),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('Gadget eCalc'),
+                    onTap: () {
+                      // Gadget 1 Aktion
+                    },
+                  ),
+                  ListTile(
+                    title: Text('Gadget Gebühren'),
+                    onTap: () {
+                      // Gadget 2 Aktion
+                    },
+                  ),
+                ],
+              ),
+            ),
+            body: Center(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('images/startcover.jpg'),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "Suchen",
+                    ),
+                  ),
+                  // Weitere Widgets für Ihre Gadgets
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            right: 20.0, // Position von rechts
+            top: 200.0, // Position von oben
+            child: Opacity(
+              opacity: _opacity,
+              child: Container(
+                width: 100.0, // Breite des Widgets
+                height: 200.0, // Höhe des Widgets
+                color: Colors.blue, // Farbe des Widgets
+                child: Center(
+                  child: Text(
+                    'Ihr Text hier', // Der Text, der im Widget angezeigt wird
+                    style: TextStyle(
+                      color: Colors.white, // Farbe des Textes
+                      fontSize: 16.0, // Größe des Textes
+                    ),
+                  ),
                 ),
               ),
-              ListTile(
-                title: Text('Gadget 1'),
-                onTap: () {
-                  // Gadget 1 Aktion
-                },
-              ),
-              ListTile(
-                title: Text('Gadget 2'),
-                onTap: () {
-                  // Gadget 2 Aktion
-                },
-              ),
-            ],
-          ),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Image.network('URL Ihres Bildes'), // Bild im Header
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "Suchen",
-                ),
-              ),
-              // Weitere Widgets für Ihre Gadgets
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          )
+        ]));
   }
 }
-
-
-/*
-class _StartPageState extends State<_StartPageState> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'eGadget',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('eGadget Home Page'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.account_circle),
-              onPressed: () {
-                // Profil-Icon Aktion
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.access_time),
-              onPressed: () {
-                // Uhr-Icon Aktion
-              },
-            ),
-          ],
-        ),
-        drawer: Drawer(
-          // Menü auf der linken Seite
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Text('Menü'),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-              ListTile(
-                title: Text('Gadget 1'),
-                onTap: () {
-                  // Gadget 1 Aktion
-                },
-              ),
-              ListTile(
-                title: Text('Gadget 2'),
-                onTap: () {
-                  // Gadget 2 Aktion
-                },
-              ),
-            ],
-          ),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Image.network('URL Ihres Bildes'), // Bild im Header
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "Suchen",
-                ),
-              ),
-              // Weitere Widgets für Ihre Gadgets
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class StartPage {}
-
-/*
-class _StartPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'eGadget',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('eGadget Home Page'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.account_circle),
-              onPressed: () {
-                // Profil-Icon Aktion
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.access_time),
-              onPressed: () {
-                // Uhr-Icon Aktion
-              },
-            ),
-          ],
-        ),
-        drawer: Drawer(
-          // Menü auf der linken Seite
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Text('Menü'),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-              ListTile(
-                title: Text('Gadget 1'),
-                onTap: () {
-                  // Gadget 1 Aktion
-                },
-              ),
-              ListTile(
-                title: Text('Gadget 2'),
-                onTap: () {
-                  // Gadget 2 Aktion
-                },
-              ),
-            ],
-          ),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Image.network('URL Ihres Bildes'), // Bild im Header
-              TextField(
-                decoration: InputDecoration(
-                  labelText: "Suchen",
-                ),
-              ),
-              // Weitere Widgets für Ihre Gadgets
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-*/
-
-*/
